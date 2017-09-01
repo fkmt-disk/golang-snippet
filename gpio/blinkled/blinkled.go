3eb4a5a313edf1efabdca30f494299148677d133
fkmt.disk <fkmt.disk@gmail.com>
2017-09-01 17:35:47 +0900

    format

--- gpio/blinkled/blinkled.go
+++ gpio/blinkled/blinkled.go
@@ -8,54 +8,55 @@ stop: Ctrl-C
 package main
 
 import (
-  "fmt"
-  "os"
-  "os/signal"
-  "syscall"
-  "time"
-  "github.com/stianeikeland/go-rpio"
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-  fmt.Println("start")
+	fmt.Println("start")
 
-  err := rpio.Open()
+	err := rpio.Open()
 
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-  stop_chan := make(chan bool, 1)
+	stop_chan := make(chan bool, 1)
 
-  signal_chan := make(chan os.Signal, 1)
-  signal.Notify(signal_chan, syscall.SIGINT)
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, syscall.SIGINT)
 
-  go func() {
-    s := <-signal_chan
-    switch s {
-      case syscall.SIGINT:
-        fmt.Println("SIGINT")
-        stop_chan <- true
-      }
-  }()
+	go func() {
+		s := <-signal_chan
+		switch s {
+		case syscall.SIGINT:
+			fmt.Println("SIGINT")
+			stop_chan <- true
+		}
+	}()
 
-  pin := rpio.Pin(21)
+	pin := rpio.Pin(21)
 
-  pin.Output()
+	pin.Output()
 
-  LOOP: for {
-    select {
-      case stop := <-stop_chan:
-        if stop {
-          break LOOP
-        }
-      default:
-        pin.Toggle()
-    }
-    time.Sleep(1 * time.Second)
-  }
+LOOP:
+	for {
+		select {
+		case stop := <-stop_chan:
+			if stop {
+				break LOOP
+			}
+		default:
+			pin.Toggle()
+		}
+		time.Sleep(1 * time.Second)
+	}
 
-  rpio.Close()
-  fmt.Println("stop")
+	rpio.Close()
+	fmt.Println("stop")
 }
